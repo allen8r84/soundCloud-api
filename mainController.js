fd91b8f8c280a5de0f1f30c0b5aae8b4bae2a0aa
Jon <jon@allen8r.com>
2015-03-03 19:15:29 -0700

    finish step 7 - only black diamond remains

--- mainController.js
+++ mainController.js
@@ -1,9 +1,15 @@
 var app = angular.module('sounder');
 
-app.controller('mainController', function($scope, soundService) {
+app.controller('mainController', function($scope, $sce, soundService) {
     $scope.getUser = function(){
         soundService.getUser($scope.userSearch).then(function(data){
             $scope.userData = data.data;
         });
     };
+    $scope.play = function(track_url){
+        SC.oEmbed(track_url, { auto_play: true }, function(oEmbed) {
+      $scope.$apply($scope.player_html = $sce.trustAsHtml(oEmbed.html));
+    });
+    };
+    
 });
