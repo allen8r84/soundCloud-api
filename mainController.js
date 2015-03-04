82dcc65a874849379e9859e99e9303f553c59443
Jon <jon@allen8r.com>
2015-03-03 19:06:49 -0700

    finish step 6

--- mainController.js
+++ mainController.js
@@ -3,7 +3,7 @@ var app = angular.module('sounder');
 app.controller('mainController', function($scope, soundService) {
     $scope.getUser = function(){
         soundService.getUser($scope.userSearch).then(function(data){
-            console.log(data.data);
+            $scope.userData = data.data;
         });
     };
 });
