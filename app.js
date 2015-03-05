62c36b0d45c078dfb645a6d8605d1536586a927e
Jon <jon@allen8r.com>
2015-03-04 18:28:29 -0700

    finish

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-var app = angular.module('sounder', []);
+var app = angular.module('sounder', ['ngRoute']);
+
+app.config(function($routeProvider, $httpProvider){
+  $httpProvider.interceptors.push('httpRequestInterceptor');
+
+/*  //router here
+  $routeProvider
+  .when('/',{
+    templateUrl: 'js/home/homeTmpl.html',
+    controller: 'homeCtrl',
+    resolve: {
+        jazzData: function(teamService) {
+            return teamService.getTeamData('utahjazz');
+        },
+        lakerData: function(teamService) {
+            return teamService.getTeamData('losangeleslakers');
+        },
+        heatData: function($route, teamService) {
+            return teamService.getTeamData('miamiheat');
+        }
+    }
+  })
+  .when('/teams/:team',{
+      templateUrl: 'js/teams/teamTmpl.html',
+      controller: 'teamCtrl',
+      resolve: {
+          teamData: function($route, teamService){
+              return teamService.getTeamData($route.current.params.team);
+          }
+      }
+  })
+  .otherwise({
+    redirectTo: '/'
+  });*/
+});
